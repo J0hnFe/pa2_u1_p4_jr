33a2198db00a238d55bf7979070678245af38aa2
John <johnfe@outlook.es>
2023-05-23 09:39:32 -0500

    Primera subida

---
+++ src/main/java/com/uce/edu/ec/PaU1P4JrApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4JrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4JrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/ec/PaU1P4JrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaU1P4JrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
