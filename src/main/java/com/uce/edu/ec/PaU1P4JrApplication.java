1c663218b8c295de0bd802a99152bc0b82d7eb6f
John <johnfe@outlook.es>
2023-05-23 09:53:13 -0500

    Taller 3, comentario almuerzo

--- src/main/java/com/uce/edu/ec/PaU1P4JrApplication.java
+++ src/main/java/com/uce/edu/ec/PaU1P4JrApplication.java
@@ -1,13 +1,23 @@
 package com.uce.edu.ec;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaU1P4JrApplication {
+public class PaU1P4JrApplication implements CommandLineRunner{ 
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaU1P4JrApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Mi primer proyecto");
+		/*
+		 * Spring framework es un permite la construccion/desarrollo
+		 * de aplicaciones basadas en Spring
+		 */
 	}
 
 }
